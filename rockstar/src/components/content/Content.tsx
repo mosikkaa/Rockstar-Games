f352fd78c93a6b434fe14a0f064865bf9df06378
Luka Meskhi <meskhiluk@gmail.com>
2025-11-27 02:32:50 +0400

    styling

--- rockstar/src/components/content/Content.tsx
+++ rockstar/src/components/content/Content.tsx
@@ -2,12 +2,12 @@ import GTA5 from '../../assets/GTA5.svg'
 import GTAOnline from '../../assets/GTAOnline.svg'
 import REDII from '../../assets/REDII.svg'
 import REDOnline from '../../assets/REDOnline.svg'
-import GTA6Game from '../../assets/GTA6Game.svg'
-import GTACAR from '../../assets/GTACAR.svg'
-import rockstarTrailer from '../../assets/ROCKSTARTrailer.svg'
+import GTACAR from '../../assets/gtacar2.jpg'
+import rockstarTrailer from '../../assets/rockstar.jpg'
 import backgroundColor from '../../assets/backgroundColor.svg'
 import gamesLauncher from '../../assets/gamesLauncher.svg'
 import secondLogo from '../../assets/secondLogo.svg'
+import GTA6GameCover from '../../assets/gta6cover.jpg'
 
 
 const Content = () => {
@@ -18,8 +18,24 @@ const Content = () => {
         )
     }
 
+    function GetCover(props:any){
+        return(
+            <div className=''>
+                <div className='w-full overflow-hidden'>
+                    <img className='w-full h-full object-cover rounded-t-2xl rounded-tl-2xl' src={props.image} alt={props.alt}/>
+                </div>
+
+                <div className='flex bg-black p-[5%] flex-col gap-3 rounded-b-2xl rounded-bl-2xl'>
+                    <h1 className='text-white text-[13px] leading-[150%] font-medium'>{props.name}</h1>
+                    <span className='text-[#797979] text-[12px] leading-[150%] font-light'>{props.description}</span>
+                </div>
+            </div>
+        )
+    }
+
+
     return (
-        <main className='bg-[#101010] w-full pt-10 pb-10 pl-[6%] pr-[6%] flex flex-col justify-between gap-16'>
+        <main className='bg-[#101010] w-full pt-18 pb-18 pl-[6%] pr-[6%] flex flex-col justify-between gap-16'>
 
             <section className='w-full flex flex-col items-center justify-between gap-8'>
 
@@ -40,70 +56,44 @@ const Content = () => {
                 </div>
 
                 <button className='text-black bg-[#FCAF17] pr-8 pl-8 pt-3 pb-3 rounded-lg'>Ver todos os jogos</button>
-
             </section>
 
-            <section className='w-full flex flex-col justify-between gap-12'>
-
+            <section className='w-full flex flex-col gap-12'>
                 <h1 className='text-5xl font-extrabold text-white'>Últimas Notícias</h1>
 
-                <div className='flex items-center gap-16 justify-between'>
+                <div className='flex gap-4'>
 
-                    <div className='flex flex-col items-start w-2/3'>
-                        <img className='rounded-t-2xl rounded-tl-2xl' src={GTA6Game} alt='GTA6Game'/>
-
-                        <div className='pl-10 w-full bg-black rounded-b-2xl rounded-bl-2xl pb-6 pt-6'>
-                            <h1 className='text-white font-medium text-[32px] leading-[150%]'>Grand Theft Auto VI - Veja o 1º trailer agora</h1>
-                            <span className='text-[#797979] font-light text-[16px] leading-[150%]'>Rockstar Games 5 de dezembro de 2023</span>
+                    <div className='rounded-2xl w-2/3'>
+                        <div className='w-full'>
+                            <div className='w-full overflow-hidden'>
+                                <img className='w-full rounded-t-2xl object-cover rounded-tl-2xl' src={GTA6GameCover} alt='GTA6Game'/>
+                                <div className='flex bg-black p-[8%] flex-col gap-3 rounded-b-2xl rounded-bl-2xl'>
+                                    <h1 className='text-white text-[32px] leading-[150%] font-medium'>Grand Theft Auto VI - Veja o 1º trailer agora</h1>
+                                    <span className='text-[#797979] text-[16px] leading-[150%] font-light'>Rockstar Games 5 de dezembro de 2023</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
-                    <div className='flex flex-col gap-16 w-1/3'>
+                    <div className='rounded-2xl w-1/3 flex flex-col gap-4 justify-between'>
 
-                        <div className='flex flex-col items-start'>
-                            <img className='rounded-t-2xl rounded-tl-2xl w-full' src={rockstarTrailer} alt='GTA6Game'/>
-
-                            <div className='pl-6 w-full bg-black rounded-b-2xl rounded-bl-2xl pb-6 pt-6'>
-                                <h1 className='text-white font-medium text-[13px] leading-[150%]'>1º Trailer. Terça-feira, 5 de Dezembro. 9h Et.</h1>
-                                <span className='text-[#797979] font-light text-[12px] leading-[150%]'>Rockstar Games 5 de dezembro de 2023</span>
-                            </div>
-                        </div>
-
-                        <div className='flex flex-col items-start'>
-                            <img className='rounded-t-2xl rounded-tl-2xl w-full ' src={GTACAR} alt='GTA6Game'/>
-
-                            <div className='pl-6 w-full bg-black rounded-b-2xl rounded-bl-2xl pb-6 pt-6'>
-                                <h1 className='text-white font-medium text-[13px] leading-[150%]'>1º Trailer. Terça-feira, 5 de Dezembro. 9h Et.</h1>
-                                <span className='text-[#797979] font-light text-[12px] leading-[150%]'>Rockstar Games 5 de dezembro de 2023</span>
-                            </div>
-                        </div>
-
-
+                        <GetCover image={rockstarTrailer} alt='rockstarTrailer' name='Grand Theft Auto VI - Veja o 1º trailer agora' description='Rockstar Games 5 de dezembro de 2023'/>
+                        <GetCover image={GTACAR} alt='GTACAR' name='Nova atualização de GTA Online chegando em dezembro' description='GTA Online      30 de novembro de 2023'/>
 
                     </div>
-
                 </div>
-
-
             </section>
 
-            <section style={{backgroundImage:`url(${backgroundColor})`}} className={`w-full bg-no-repeat bg-cover flex items-center justify-between pt-10 pb-10 pl-[4%] rounded-2xl`}>
-
+            <section style={{backgroundImage:`url(${backgroundColor})`}} className={`w-full bg-no-repeat bg-cover sm:flex-col md:flex-row flex items-center justify-between pt-10 pb-10 pl-[4%] rounded-2xl`}>
                 <div className='flex flex-col gap-6 items-start justify-between'>
-
                     <div className='flex-col lg:items-center lg:flex-row flex items-start gap-6 font-extrabold text-[41.57px] leading-[120%] '>
                         <img src={secondLogo} alt='secondLogo'/>
                         <h1>Games <br/> Launcher</h1>
                     </div>
-
                     <span className='font-medium text-[32px] leading-[150%]'>Todos os seus jogos <br/> em um só lugar.</span>
-
                     <button className='rounded-lg bg-[#FCAF17] shadow-[0px 4px 16px 1px #00000052] font-extrabold text-[20px] leading-[150%] pr-18 pl-18 pt-4 pb-4'>Download</button>
-
                 </div>
-
-                    <img className='w-[60%] h-[80%]' src={gamesLauncher} alt='secondLogo'/>
-
+                <img className='sm:w-full md:w-[60%] h-[80%]' src={gamesLauncher} alt='secondLogo'/>
             </section>
 
 
